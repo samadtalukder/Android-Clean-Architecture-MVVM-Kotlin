5c7207ecf889f9d9dbcffaa3a199a6cea5419406
Samad Talukder <tsamad@hlbb.hongleong.com.my>
2022-07-25 01:58:20 +0800

    constants file created

---
+++ app/src/main/java/com/samad_talukder/androidcleanarchitecturemvvm/common/utils/Constants.kt
@@ -0,0 +1,13 @@
+package com.samad_talukder.androidcleanarchitecturemvvm.common.utils
+
+/**
+ * Created by Samad Talukder on 10, June, 2022.
+ * github.com/samadtalukder
+ **/
+
+object Constants {
+
+    const val CONNECTION_TIMEOUT = 45
+    const val READ_TIMEOUT = 45
+    const val WRITE_TIMEOUT = 45
+}
